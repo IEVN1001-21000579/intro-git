c0b5de51d578cca1677f1720c3033c50d2b77e8d
cesar uriel isaac <21000579@alumnos.utleon.edu.mx>
2024-09-09 20:13:54 -0600

    quinto commit

--- intro-ts/src/temas-ts/01-tipos-bas.ts
+++ intro-ts/src/temas-ts/01-tipos-bas.ts
@@ -14,3 +14,11 @@ console.log(suma)
 let correo:string;
 let age:number;
 let activo:boolean;
+
+correo='xx@gmail.com'
+age=12
+activo=true
+
+let matricula:number|string;
+matricula=21000579
+matricula='21000579'
