c171edfd8c25981c86d7f59968a87f2d4d55c8f1
cesar uriel isaac <21000579@alumnos.utleon.edu.mx>
2024-09-09 20:12:04 -0600

    cuarto commit

--- intro-ts/src/temas-ts/01-tipos-bas.ts
+++ intro-ts/src/temas-ts/01-tipos-bas.ts
@@ -1,2 +1,16 @@
 //console.log('hello world')
 
+var nombre = 'César'
+let apellido = 'Perez'
+const edad = 30
+
+let num1=2
+let num2='fdshbfjh'
+let num3=3
+
+let suma = num1 +num2 +num3
+console.log(suma)
+
+let correo:string;
+let age:number;
+let activo:boolean;
